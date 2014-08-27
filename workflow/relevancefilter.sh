55bc3d3bf84520cdeaebd1f7bd73e4e07b929ab9
eden-06 <thomas.eden06.kuehn@googlemail.com>
2014-08-27 17:25:08 +0200

    fixed issue 1

--- workflow/relevancefilter.sh
+++ workflow/relevancefilter.sh
@@ -7,10 +7,11 @@
 # a ten year old publications would would have at least.
 
 CONSTANT="12"
+YEAR=`date +%Y`
 
 for f in `ls filter_pub/` ; do
  i="$(sed -r 's/paper([0-9]+)[.]bib/\1/' <<< $f)" ; 
- CITATIONCOUNT=`echo "a=$CONSTANT*(l(2014-$i)/l(10)); scale=0; if(a>0){ (a+0.5)/1 }else{ 0 }" | bc -l` ;
+ CITATIONCOUNT=`echo "a=$CONSTANT*(l($YEAR-$i)/l(10)); scale=0; if(a>0){ (a+0.5)/1 }else{ 0 }" | bc -l` ;
  echo " filter citations from the year $i below the citation limit of $CITATIONCOUNT"
  ruby bibfilter.rb -c$CITATIONCOUNT "filter_pub/$f" > "filter_rel/$f" ;
 done
