bf1f3ba0ff23933767bf77ec0d8c9ef9f0a513d7
eden-06 <thomas.eden06.kuehn@googlemail.com>
2016-11-01 16:18:18 +0100

    added RUBY constant to relevance filter

--- workflow/relevancefilter.sh
+++ workflow/relevancefilter.sh
@@ -8,10 +8,11 @@
 
 CONSTANT="12"
 YEAR=`date +%Y`
+RUBY="ruby1.9.1"
 
 for f in `ls filter_pub/` ; do
  i="$(sed -r 's/paper([0-9]+)[.]bib/\1/' <<< $f)" ; 
  CITATIONCOUNT=`echo "a=$CONSTANT*(l($YEAR-$i)/l(10)); scale=0; if(a>0){ (a+0.5)/1 }else{ 0 }" | bc -l` ;
  echo " filter citations from the year $i below the citation limit of $CITATIONCOUNT"
- ruby bibfilter.rb -c$CITATIONCOUNT "filter_pub/$f" > "filter_rel/$f" ;
+ $RUBY bibfilter.rb -c$CITATIONCOUNT "filter_pub/$f" > "filter_rel/$f" ;
 done
