693a0d393b07b980c7b1ec0b695a6656a57621da
Eden-06 <thomas.eden06.kuehn@googlemail.com>
2018-02-09 18:21:02 +0100

    add documentaiton to test module

--- gsresearch/test-module.rb
+++ gsresearch/test-module.rb
@@ -19,10 +19,14 @@ EOS
 		@query=nil
 	end
 
+	#called before querying to set up a connection to the publication site
 	def prepare()
 		puts "Preparing Crawler" if @verbose
 	end
 
+	#called after prepare and acutally queries the publication site and calls proc
+	#for each found publication providing the bibtex string and/or an error
+	#message.
 	def query(query,&proc)
 		if proc.nil? or not proc.respond_to?(:call)
 			throw ArgumentError.new("query() must be called with a block, lambda or callable procedure!")
@@ -32,10 +36,12 @@ EOS
 		10.times do
 			proc.call(nil,"[Warning]: This is just a dummy implementation!") 	
 		end
+		nil
 	end
 
 end
 
+# register module
 Modules["test"]=lambda do|agent,verbose|
 	DummyCrawler.new(agent,verbose)
 end
